284c96828d9ff87381cf178edcf0e9af7b8b26e9
lly <2827301517@qq.com>
2025-06-30 19:20:00 +0800

    init

---
+++ src/main/java/com/lly/shorturlx/ShorturlxApplication.java
@@ -0,0 +1,13 @@
+package com.lly.shorturlx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorturlxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShorturlxApplication.class, args);
+    }
+
+}
